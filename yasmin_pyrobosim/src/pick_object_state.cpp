83ee6fd578d161a2ff5a281d10cfb327e2162422
Miguel Ángel González Santamarta <mgons@unileon.es>
2025-11-13 18:13:56 +0100

    creating pick and place states

---
+++ yasmin_pyrobosim/src/pick_object_state.cpp
@@ -0,0 +1,32 @@
+
+#include <memory>
+#include <string>
+
+#include "pyrobosim_msgs/action/execute_task_action.hpp"
+#include "yasmin/state.hpp"
+#include "yasmin_ros/action_state.hpp"
+
+#include <pluginlib/class_list_macros.hpp>
+
+using ExecuteTaskAction = pyrobosim_msgs::action::ExecuteTaskAction;
+using namespace std::placeholders;
+
+class PickObjectState : public yasmin_ros::ActionState<ExecuteTaskAction> {
+
+public:
+  PickObjectState()
+      : yasmin_ros::ActionState<ExecuteTaskAction>(
+            "/execute_action",
+            std::bind(&PickObjectState::create_goal_handler, this, _1)){};
+
+  ExecuteTaskAction::Goal create_goal_handler(
+      std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) {
+
+    auto goal = ExecuteTaskAction::Goal();
+    goal.action.type = "pick";
+    goal.action.object = blackboard->get<std::string>("object");
+    return goal;
+  };
+};
+
+PLUGINLIB_EXPORT_CLASS(PickObjectState, yasmin::State)
---
+++ yasmin_pyrobosim/src/place_object_state.cpp
@@ -0,0 +1,31 @@
+
+#include <memory>
+#include <string>
+
+#include "pyrobosim_msgs/action/execute_task_action.hpp"
+#include "yasmin/state.hpp"
+#include "yasmin_ros/action_state.hpp"
+
+#include <pluginlib/class_list_macros.hpp>
+
+using ExecuteTaskAction = pyrobosim_msgs::action::ExecuteTaskAction;
+using namespace std::placeholders;
+
+class PlaceObjectState : public yasmin_ros::ActionState<ExecuteTaskAction> {
+
+public:
+  PlaceObjectState()
+      : yasmin_ros::ActionState<ExecuteTaskAction>(
+            "/execute_action",
+            std::bind(&PlaceObjectState::create_goal_handler, this, _1)){};
+
+  ExecuteTaskAction::Goal create_goal_handler(
+      std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) {
+
+    auto goal = ExecuteTaskAction::Goal();
+    goal.action.type = "place";
+    return goal;
+  };
+};
+
+PLUGINLIB_EXPORT_CLASS(PlaceObjectState, yasmin::State)
