71d97525651e1a4a2adeb80996adce6f27315f54
Miguel Ángel González Santamarta <mgons@unileon.es>
2025-11-13 18:28:39 +0100

    adding detected objets to the blackboard

--- yasmin_pyrobosim/src/detect_objects_state.cpp
+++ yasmin_pyrobosim/src/detect_objects_state.cpp
@@ -3,8 +3,10 @@
 #include <string>
 
 #include "pyrobosim_msgs/action/detect_objects.hpp"
+#include "pyrobosim_msgs/msg/object_state.hpp"
 #include "yasmin/state.hpp"
 #include "yasmin_ros/action_state.hpp"
+#include "yasmin_ros/basic_outcomes.hpp"
 
 #include <pluginlib/class_list_macros.hpp>
 
@@ -17,7 +19,8 @@ public:
   DetectObjectsState()
       : yasmin_ros::ActionState<DetectObjectsAction>(
             "detect_objects",
-            std::bind(&DetectObjectsState::create_goal_handler, this, _1)){};
+            std::bind(&DetectObjectsState::create_goal_handler, this, _1),
+            std::bind(&DetectObjectsState::on_result, this, _1, _2)){};
 
   DetectObjectsAction::Goal create_goal_handler(
       std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) {
@@ -29,6 +32,16 @@ public:
     }
 
     return goal;
+  };
+
+  std::string
+  on_result(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard,
+            const typename DetectObjectsAction::Result::SharedPtr result) {
+
+    blackboard->set<std::vector<pyrobosim_msgs::msg::ObjectState>>(
+        "detected_objects", result->detected_objects);
+
+    return yasmin_ros::basic_outcomes::SUCCEED;
   };
 };
 
